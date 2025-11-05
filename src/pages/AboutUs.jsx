c411b4db2b9c34bf496f181f63a3618a1582aaf2
YunseoChoi0919 <144880431+YunseoChoi0919@users.noreply.github.com>
2025-11-05 15:42:58 +0900

    소녀의 작품 빼고 완성

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,5 +1,5 @@
 import Header from "../components/Header";
-// import "../styles.css";
+import "../styles.css";
 
 import Footer from "../components/Footer";
 
@@ -8,6 +8,9 @@ import "./ProjectIntro.css";
 import ys from "../assets/profiles/ys.jpeg";
 import BackButton from "../components/BackButton";
 
+import dh from "../assets/profiles/dh.jpeg";
+import sy from "../assets/profiles/sy.jpeg";
+
 
 
 
@@ -15,16 +18,16 @@ export default function AboutUs() {
     return <div className="artworks-page" id="about-us-content">
         <Header />
         <div className="project-intro-content" >
-          <div className="main-title">팀 기록도록 소개</div>
+          <div className="main-title">팀 기록도록 </div>
 
     
           <div className="intro-content">&nbsp; &nbsp; 팀 기록도록은 2025 Art with Impact 프로그램 참여를 위해 모인 팀입니다.
              평소에 모두 서강대학교 순수 미술 동아리인 [강미반] 소속으로 친하게 지내면서 자연스럽게 모이게 되었습니다.
-              "너무 복잡하지 않게, 하지만 재밌는 것을 하자"는 것이 저희의 모토입니다. 
+              <span>"너무 복잡하지 않게, 하지만 재밌는 것을 하자"</span>는 것이 저희의 모토입니다. 
 
       <br></br> </div>
     
-      <div className="main-title">팀원 소개</div>
+      <div className="main-title">팀원 </div>
       <div className="members">
 
         <div className="member">
@@ -33,7 +36,14 @@ export default function AboutUs() {
           </div>
           <div className="profile-contents">
             <div className="member-name">최윤서 (Lead)</div>
-            <div className="member-intro">아트와 테크의 가운데를 잘 파고들고 싶은 최윤서입니다. 프론트엔드, 유니티, 일러스트 작업, 기계공학 등 잡다한 관심사가 있습니다.</div>
+            <div className="member-intro">아트와 테크의 가운데를 잘 파고들고 싶은 최윤서입니다. 프론트엔드, 유니티, 일러스트 작업, 기계공학 등 잡다한 관심사가 있습니다.
+
+            </div>
+            <div className="contact">
+              <div className="mail">yschoi0919@gmail.com</div>
+              <div className="mail">@yschoi0919</div>
+
+            </div>
           </div>
         </div>
 
@@ -41,11 +51,18 @@ export default function AboutUs() {
 
                 <div className="member">
           <div className="profile">
-            <img src={ys} width="100px"alt="" />
+            <img src={dh} width="100px"alt="" />
           </div>
           <div className="profile-contents">
-            <div className="member-name">최윤서 (Lead)</div>
-            <div className="member-intro">아트와 테크의 가운데를 잘 파고들고 싶은 최윤서입니다. 프론트엔드, 유니티, 일러스트 작업, 기계공학 등 잡다한 관심사가 있습니다.</div>
+            <div className="member-name"> 김대희</div>
+            <div className="member-intro">서강대학교 아트&테크놀로지 학과 23학번 김대희입니다.
+              게임 개발관련해서 관심이 많습니다.
+            </div>
+           <div className="contact">
+              <div className="mail">kdaehee1021@gmail.com</div>
+             
+
+            </div>
           </div>
         </div>
 
@@ -54,11 +71,21 @@ export default function AboutUs() {
 
                 <div className="member">
           <div className="profile">
-            <img src={ys} width="100px"alt="" />
+            <img src={sy} width="100px"alt="" />
           </div>
           <div className="profile-contents">
-            <div className="member-name">최윤서 (Lead)</div>
-            <div className="member-intro">안녕하세요! 아트와 테크의 가운데를 잘 파고들고 싶은 최윤서입니다. 프론트엔드, 유니티, 일러스트 작업, 기계공학 등 잡다한 관심사가 있습니다.</div>
+            <div className="member-name">김시윤</div>
+            <div className="member-intro">
+              팀 내에서 유치원 교사를 담당하고 있습니다.
+            교환학생 중인데 초딩같은 팀원들이 걱정이에요.
+            </div>
+
+                       <div className="contact">
+              <div className="mail">tldbs37@gmail.com</div>
+              <div className="mail">@nuy.lv_</div>
+             
+            </div>
+            
           </div>
         </div>
 
@@ -74,17 +101,20 @@ export default function AboutUs() {
 Project Director - 최윤서<br></br>
 Concept Development - 김대희, 김시윤, 최윤서<br></br>
 Narrative Design & Storyboarding - 최윤서<br></br>
+<br></br>
 
 <span>Graphic</span><br></br>
 UI Design - 최윤서<br></br>
 Scene Design - 김대희, 최윤서<br></br>
 Character Design & Animation - 김시윤<br></br>
+<br></br>
 
  <span>Program</span><br></br>
 C# Programming - 김대희, 김시윤, 최윤서<br></br>
 Database Development - 최윤서<br></br>
 AI Prompt Engineering - 김대희, 김시윤, 최윤서<br></br>
 Web Development - 최윤서<br></br>
+<br></br>
 
 
  <span>Sound</span><br></br>
